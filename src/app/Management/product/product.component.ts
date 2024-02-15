5bfbc6ffc5478aff6e74b160f3ab15f2c05965ce
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2024-02-15 14:25:06 +0700

    thêm sản phẩm

--- src/app/Management/product/product.component.ts
+++ src/app/Management/product/product.component.ts
@@ -13,16 +13,17 @@ import { DanhMucService } from '../../Services/servicesDanhMuc/danh-muc.service'
 export class ProductComponent implements OnInit {
   SanPham: any[] = [];
   DanhMuc: any[] = [];
+  HinhAnhSanPham: any[] = [];
   MaSanPham: string = '';
   TenSanPham: string = '';
   SoLuong: number = 1;
   DonGia: number = 0;
   MoTa: string = '';
   Hang: string = '';
-  HinhAnh: any;
   MaDanhMuc: string = '';
   GiamGia: number = 0;
   MaNguoiDung: string = '';
+  selectedFile: FileList | null = null;
 
   constructor(
     private sanPhamServices: ServiceSanPhamService,
@@ -36,6 +37,9 @@ export class ProductComponent implements OnInit {
       this.DanhMuc = data;
       this.MaDanhMuc = data[0].MaDanhMuc;
     });
+  }
+  inputFileAnh(event: any) {
+    this.selectedFile = event.target.files;
   }
   ThayDoiMaRandom() {
     const MaRandom = this.sanPhamServices.randomMa();
@@ -58,7 +62,10 @@ export class ProductComponent implements OnInit {
         GiamGia: this.GiamGia,
         MoTa: this.MoTa,
         MaNguoiDung: this.MaNguoiDung,
+
       };
+
+
 
       const response = await fetch('http://localhost:4000/sanpham', {
         method: 'POST',
@@ -72,6 +79,45 @@ export class ProductComponent implements OnInit {
         throw new Error(`HTTP error! status: ${response.status}`);
       } else {
         console.log(response);
+      }
+
+
+
+      if (this.selectedFile) {
+        const countFile = this.selectedFile.length;
+        const formDataImg = new FormData();
+        for (let i = 0; i < countFile; i++) {
+          formDataImg.append('files', this.selectedFile[i]);
+        }
+        try {
+          const response = await fetch("http://localhost:4000/multifiles", {
+            method: 'POST',
+            body: formDataImg
+          });
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          } else {
+
+            const formData = new FormData();
+            for (let i = 0; i < countFile; i++) {
+              const DataHinhAnhSanPham = {
+                MaSanPham: this.MaSanPham,
+                TenFileAnh: this.selectedFile[i].name
+              }
+              this.HinhAnhSanPham.push(DataHinhAnhSanPham);
+              formData.append('files', this.selectedFile[i]);
+              formData.append('MaSanPham', this.MaSanPham);
+            }
+            console.log(this.HinhAnhSanPham);
+            const response = await fetch('http://localhost:4000/hinhanh', {
+              method: "POST",
+              body: formData
+            });
+          }
+        }
+        catch (error) {
+          console.error('There was a problem with the fetch operation: ', error);
+        }
       }
     } catch (error) {
       console.error('An error occurred while adding the product:', error);
